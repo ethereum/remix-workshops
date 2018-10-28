e4d4f65f984ea0007b743072f5f4add0f991a756
yann300 <yann.levreau@gmail.com>
2018-10-28 12:14:52 +0100

    Update loadAwardProxyDependencies.js

--- runningScript/proxyContract_AwardToken/loadAwardProxyDependencies.js
+++ runningScript/proxyContract_AwardToken/loadAwardProxyDependencies.js
@@ -1,4 +1,5 @@
 remix.loadurl('https://github.com/ethereum/remix-workshops/runningScript/proxyContract_AwardToken/AwardTokenProxy.sol')
+remix.loadurl('https://github.com/ethereum/remix-workshops/runningScript/proxyContract_AwardToken/AwardTokenProxy.json')
 remix.loadurl('https://github.com/ethereum/remix-workshops/runningScript/proxyContract_AwardToken/AwardToken.sol')
 remix.loadurl('https://github.com/ethereum/remix-workshops/runningScript/proxyContract_AwardToken/Ballot.sol')
 remix.loadurl('https://github.com/ethereum/remix-workshops/runningScript/proxyContract_AwardToken/ERC20Mintable.sol')
