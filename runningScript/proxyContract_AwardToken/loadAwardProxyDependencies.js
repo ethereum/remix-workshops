aa5403f18995259de813b9fd39c680c116fe619c
yann300 <yann.levreau@gmail.com>
2018-10-28 11:26:58 +0100

    Update loadAwardProxyDependencies.js

--- runningScript/proxyContract_AwardToken/loadAwardProxyDependencies.js
+++ runningScript/proxyContract_AwardToken/loadAwardProxyDependencies.js
@@ -1,2 +1,3 @@
 remix.loadurl('https://github.com/ethereum/remix-workshops/runningScript/proxyContract_AwardToken/AwardTokenProxy.sol')
 remix.loadurl('https://github.com/ethereum/remix-workshops/runningScript/proxyContract_AwardToken/AwardToken.sol')
+remix.loadurl('https://github.com/ethereum/remix-workshops/runningScript/proxyContract_AwardToken/Ballot.sol')
