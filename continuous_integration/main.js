eae956ce58529f4c0802814d0fbc94e2c082d024
yann300 <yann.levreau@gmail.com>
2018-10-28 19:19:26 +0100

    Create main.js

---
+++ continuous_integration/main.js
@@ -0,0 +1,63 @@
+var remixSolidity = require('remix-solidity')
+var CodeAnalysis = require('remix-analyzer').CodeAnalysis
+var remixTests = require('remix-tests')
+var Provider = require('remix-simulator').Provider
+var Web3 = require ('web3')
+var fs = require('fs');
+ 
+var resolver = (importUrl) => {
+    // called by the compiler for resolving sources.
+}
+
+var compiler = new remixSolidity.Compiler(resolver)
+compiler.onInternalCompilerLoaded()
+
+compiler.event.register('compilationFinished', this, function (success, data, source) {
+    if (!success) {
+        console.error('\x1b[31m%s\x1b[0m', data)
+    } else {
+        console.log(data, source)
+        runStaticAnalysis(data)  
+        runTests()
+    }
+})
+
+var runStaticAnalysis = (data) => {
+    var analysis = new CodeAnalysis
+    var modulesToRun = []
+    console.log('running static analysis modules:')
+    analysis.modules().forEach((element, index) => {
+        // select which module to run:
+        if (element.algorithm && element.algorithm.id === 'EXACT') {
+            modulesToRun.push(index)
+            console.log('\x1b[33m%s\x1b[0m', element.name + ' ' + element.description)
+        }
+    });
+    analysis.run(data, modulesToRun, function (results) {
+        console.log('static analysis result: ')
+        var result = false
+        results.forEach((element, index) => {
+            element.report.forEach(element => {
+                console.log('\x1b[31m%s\x1b[0m', JSON.stringify(element))
+                result = true
+            })
+        })
+        if (!result) { console.log('No warning')}
+    })
+}
+
+var runTests = () => {
+    let web3 = new Web3()
+    web3.setProvider(new Provider())
+    remixTests.runTestFiles('./sample_test.sol', false, web3)
+}
+
+fs.readFile('sample.sol', 'utf8', function(err, contents) {
+    var contract = {
+        'test.sol': {
+            content: contents
+        }
+    }
+    compiler.compile(contract, '') 
+});
+
