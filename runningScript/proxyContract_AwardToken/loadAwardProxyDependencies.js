c4eadc2a551e3e5c122e7673864a4835075cc9ae
yann300 <yann.levreau@gmail.com>
2018-10-28 11:14:58 +0100

    Create loadAwardProxyDependencies.js

---
+++ runningScript/proxyContract_AwardToken/loadAwardProxyDependencies.js
@@ -0,0 +1,2 @@
+remix.loadurl('https://github.com/ethereum/remix-workshops/runningScript/proxyContract_AwardToken/AwardTokenProxy.sol')
+remix.loadurl('https://github.com/ethereum/remix-workshops/runningScript/proxyContract_AwardToken/AwardToken.sol')
