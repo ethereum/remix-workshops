3dc67414506479c9053b44f11c46c5fa52b86624
LianaHus <liana@ethdev.com>
2019-08-21 09:53:31 +0200

    renamed files

---
+++ proxyContract/step6/step6.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.5.1;
+
+contract StorageContract {
+    uint counter;
+    
+}
+
+contract ProxyContract is StorageContract {
+    address internal proxied;
+    
+    function setLogicContractAddress (address newAddress) public
+    {
+        proxied = newAddress;
+    }
+    
+    constructor(address _proxied) public {
+        proxied = _proxied;
+    }
+    
+    /**
+     * Fallback function allowing to perform a delegatecall 
+     * to the given implementation. This function will return 
+     * whatever the implementation call returns
+     */
+    function () external payable {
+        address addr = proxied;
+        assembly {
+            let freememstart := mload(0x40)
+            calldatacopy(freememstart, 0, calldatasize())
+            let success := delegatecall(not(0), addr, freememstart, calldatasize(), freememstart, 32)
+            switch success
+            case 0 { revert(freememstart, 32) }
+            default { return(freememstart, 32) }
+        }
+    }    
+}
+
+contract LogicContract is StorageContract {
+
+    function getcounter () public view returns (uint)
+    {
+        return counter;
+    }
+    
+    function incrementcounter () public
+    {
+        counter++;
+    }
+}
---
+++ proxyContract/step7/step7.md
@@ -0,0 +1,19 @@
+* Check out some links for more info
+
+- ERC DelegateProxy
+https://github.com/ethereum/EIPs/pull/897
+
+- The joint post by Zeppelin Solutions and Aragon.
+https://blog.openzeppelin.com/proxy-libraries-in-solidity-79fbe4b970fd/
+
+- Proxy Patterns by OpenZeppelin
+https://blog.openzeppelin.com/proxy-patterns/
+
+- The most mature implementation of Proxy pattern is The Unstructure Storage pattern that is released as part of ZeppelinOS 
+https://blog.openzeppelin.com/upgradeability-using-unstructured-storage/
+
+- Great blog post by Jack Tanner
+https://blog.indorse.io/ethereum-upgradeable-smart-contract-strategies-456350d0557c
+
+- Upgradeable Ethereum Smart Contracts
+https://medium.com/@daonomic/upgradeable-ethereum-smart-contracts-d036cb373d6
