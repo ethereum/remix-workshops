699f97815652d4c64ea5e5db795daf184ef81226
Rob Stupay <rob@ethereum.org>
2020-05-28 01:42:17 -0400

    cleanup of folder names in solidity intro

--- Solidity-intro/2_variables/variables_answer.sol
+++ Solidity-intro/2_variables/variables_answer.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.0 <0.7.0;
 contract SimpleStorage {
     uint256 storedData = 3;
     
-   function get() public view returns (uint256 retVal) {
+   function get() public view returns (uint256) {
     return storedData;
   }
 }
