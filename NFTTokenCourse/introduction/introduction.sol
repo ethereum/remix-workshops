d4fca0e96379c0529728eff5f99475a620a9f625
Rob Stupay <bcamionette@unknown064f67fb7d51.attlocal.net>
2022-02-10 15:01:05 -0500

    add keyword public

--- NFTTokenCourse/introduction/introduction.sol
+++ NFTTokenCourse/introduction/introduction.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.3;
 
 contract Quiz {
-    uint8 question1 = 0;
-    uint8 question2 = 0;
-    uint8 question3 = 0;
+    uint8 public question1 = 0;
+    uint8 public question2 = 0;
+    uint8 public question3 = 0;
 }
