b053a15ba1ada0e9263b9aff667707763be5541a
ryestew <rob@stupay.com>
2024-10-25 23:08:38 -0400

    Update DepolyWith

---
+++ DeployWithLibraries/1_Writing_a_Library/1_contractSimpleLibrary_test.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.4.22 <0.6.0;
+import "remix_tests.sol"; // this import is automatically injected by Remix.
+import "./1_contractSimpleLibrary.sol";
+
+contract test3 {
+   
+    test useSimpleLibrary;
+    function beforeAll () public {
+       useSimpleLibrary = new test();
+    }
+    
+    function useSimpleLibraryTest () public {
+        Assert.equal(LibraryForTest.getFromLib(), uint(3), "the implemented library should return 3");
+        Assert.equal(useSimpleLibrary.get(), uint(3), "the implemented library should return 3");
+    }
+}
